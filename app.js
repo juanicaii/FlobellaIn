7c2b1a2b9d2a417b9c01de15d21417fce0af63ea
juaniseijas <juaniseijas00@gmail.com>
2021-01-12 09:07:18 -0300

    fix

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use("/", mainRoutes);
 app.get("*", (req, res) => {
-  const index = path.join(__dirname, "dist/build", "index.html");
+  res.sendFile("./dist/build/index.html");
 });
 app.listen(3200);
 module.exports = app;
