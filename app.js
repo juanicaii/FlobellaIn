09c81e30d3bd7a1d3fa7396f53b0aaa7101351b5
juaniseijas <juaniseijas00@gmail.com>
2021-01-12 09:13:40 -0300

    fix

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use("/", mainRoutes);
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/front/build/index.html");
+  res.sendFile(__dirname + "build", "index.html");
 });
 app.listen(3200);
 module.exports = app;
---
+++ build/precache-manifest.6925cd9e5a14ed797a6f9d7d7cd64586.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1720baa6eec6fa12f716e4c846e8ec7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b81a7ef2a6a137e4c44",
+    "url": "/static/css/main.a7320b03.chunk.css"
+  },
+  {
+    "revision": "e14bf12c648b5a3aec5b",
+    "url": "/static/js/2.7b836b47.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.7b836b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b81a7ef2a6a137e4c44",
+    "url": "/static/js/main.75fcd8db.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "6d4e05823697958d8f798ee6f7cf3881",
+    "url": "/static/media/Aileron-Black.6d4e0582.otf"
+  },
+  {
+    "revision": "56bef66f66057990468ce5ed2397ea2b",
+    "url": "/static/media/logoJS.56bef66f.otf"
+  }
+]);
